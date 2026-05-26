6f2216948d0e4b9e86ce06caecae27d3d4f811cd
voj-tech-j <78592193+voj-tech-j@users.noreply.github.com>
2026-05-26 09:06:09 +0200

    feat: add audience API wrappers (#5)

--- src/lettr/resources/__init__.py
+++ src/lettr/resources/__init__.py
@@ -1,9 +1,10 @@
 """Lettr API resource modules."""
 
+from .audience import Audience
 from .domains import Domains
 from .emails import Emails
 from .projects import Projects
 from .templates import Templates
 from .webhooks import Webhooks
 
-__all__ = ["Domains", "Emails", "Projects", "Templates", "Webhooks"]
+__all__ = ["Audience", "Domains", "Emails", "Projects", "Templates", "Webhooks"]
